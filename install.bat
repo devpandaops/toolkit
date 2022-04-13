b3b9d6b17d6e79011fc7891271d40a33e6fddc31
samuel verneck <102948001+osamucaaaa@users.noreply.github.com>
2022-04-13 15:17:52 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,48 +6,18 @@
 
 :::: Dev tools
 choco install git.install -y
-choco install gitkraken
 choco install vscode -y
 choco install jre8 -y
-choco install iisexpress -y
-choco install mysql -y
-choco install mysql.workbench -y
 choco install postman -y
-::choco install docker-desktop -y
+choco install docker-engine -y
 choco install nodejs.install -y
 
 :::: Utilities
 choco install googlechrome -y
-choco install slack -y
 choco install zoom -y
 choco install adobereader
-choco install dropbox -y
-choco install telegram.install -y
 
 :::: Other
-::choco install lenovo-thinkvantage-system-update -y
-choco install 7zip.install -y
-choco install lightshot -y
-choco install whatsapp -y
 choco install notepadplusplus -y
 
-@REM choco install git.install -y --params "/GitAndUnixToolsOnPath /NoAutoCrlf /WindowsTerminal"
-call refreshenv
-rem always have Linux line endings in text files
-git config --global core.autocrlf input
-rem support more than 260 characters on Windows
-rem See https://stackoverflow.com/a/22575737/873282 for details
-git config --global core.longpaths true
-rem some color and diff tweaks
-rem   Use SVN's ||| also in git
-rem   See https://git-scm.com/docs/git-config#Documentation/git-config.txt-mergeconflictStyle for details
-git config --global merge.configStyle "diff3"
-rem Always push to the branch we pulled from
-rem   See https://git-scm.com/docs/git-config#Documentation/git-config.txt-pushdefault for details
-git config --global push.default upstream
-git config --global color.diff.new "green bold"
-git config --global color.status.updated "green bold"
-git config --global color.branch.current "green bold"
-rem Sort branches at "git branch -v" by committer date
-git config --global branch.sort -committerdate
 
