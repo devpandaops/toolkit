216af734ecf208a9ff3c6812ebc9732fed9a6a23
samuel verneck <102948001+osamucaaaa@users.noreply.github.com>
2022-04-16 02:38:57 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,53 +1,31 @@
 :: Install choco .exe and add choco to PATH
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
-:: Install all the packages
-:: choose: visual studio, office, .net core, .net, rider tools
-
-:::: Dev tools
-choco install git.install -y
-choco install gitkraken
-choco install vscode -y
-choco install jre8 -y
-choco install iisexpress -y
-choco install mysql -y
-choco install mysql.workbench -y
-choco install postman -y
-::choco install docker-desktop -y
-choco install nodejs.install -y
-
-:::: Utilities
-choco install googlechrome -y
-choco install slack -y
-choco install zoom -y
-choco install adobereader
-choco install dropbox -y
-choco install telegram.install -y
-
-:::: Other
-::choco install lenovo-thinkvantage-system-update -y
-choco install 7zip.install -y
-choco install lightshot -y
-choco install whatsapp -y
-choco install notepadplusplus -y
-
-@REM choco install git.install -y --params "/GitAndUnixToolsOnPath /NoAutoCrlf /WindowsTerminal"
-call refreshenv
-rem always have Linux line endings in text files
-git config --global core.autocrlf input
-rem support more than 260 characters on Windows
-rem See https://stackoverflow.com/a/22575737/873282 for details
-git config --global core.longpaths true
-rem some color and diff tweaks
-rem   Use SVN's ||| also in git
-rem   See https://git-scm.com/docs/git-config#Documentation/git-config.txt-mergeconflictStyle for details
-git config --global merge.configStyle "diff3"
-rem Always push to the branch we pulled from
-rem   See https://git-scm.com/docs/git-config#Documentation/git-config.txt-pushdefault for details
-git config --global push.default upstream
-git config --global color.diff.new "green bold"
-git config --global color.status.updated "green bold"
-git config --global color.branch.current "green bold"
-rem Sort branches at "git branch -v" by committer date
-git config --global branch.sort -committerdate
-
+choco install adobereader --version 2022.001.20085 -y
+choco install googlechrome --version 100.0.4896.127 -y
+choco install google-drive-file-stream --version 55.0.3.0 -y
+choco install notepadplusplus.install --version 8.3.3 -y
+choco install python3 --version 3.10.4 -y
+choco install microsoft-teams.install --version 1.5.00.8070 -y
+choco install git.install --version 2.35.3 -y
+choco install zoom --version 5.10.1.4420 -y
+choco install winrar --version 6.11 -y
+choco install nodejs.install --version 17.9.0 -y
+choco install vscode --version 1.66.2 -y
+choco install treesizefree --version 4.5.3 -y
+choco install curl --version 7.82.0 -y
+choco install microsoft-windows-terminal --version 1.12.10732.0 -y
+choco install awscli --version 2.5.4 -y
+choco install slack --version 4.25.2 -y
+choco install vim --version 8.2.4751 -y
+choco install yarn --version 1.22.15 -y
+choco install virtualbox --version 6.1.32 -y
+choco install golang --version 1.18.1 -y
+choco install azure-cli --version 2.35.0 -y
+choco install postman --version 9.15.2 -y
+choco install tortoisegit --version 2.13.0.1 -y
+choco install kubernetes-cli --version 1.23.5 -y
+choco install terraform --version 1.1.8 -y
+choco install docker-desktop --version 4.7.0 -y
+choco install maven --version 3.8.5 -y
+choco install pip --version 1.2.0 -y
