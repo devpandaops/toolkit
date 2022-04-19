402b4f6405f69ce4c699f4015ebe91ee4a6e57e3
samuel verneck <102948001+osamucaaaa@users.noreply.github.com>
2022-04-19 11:12:36 -0300

    Create script.bat

---
+++ script.bat
@@ -0,0 +1,24 @@
+:: Install choco .exe and add choco to PATH
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+
+choco install adobereader --version 2022.001.20085 -y
+choco install googlechrome --version 100.0.4896.127 -y
+choco install notepadplusplus.install --version 8.3.3 -y
+choco install python3 --version 3.10.4 -y
+choco install microsoft-teams.install --version 1.5.00.8070 -y
+choco install git.install --version 2.35.3 -y
+choco install zoom --version 5.10.1.4420 -y
+choco install winrar --version 6.11 -y
+choco install nodejs.install --version 17.9.0 -y
+choco install vscode --version 1.66.2 -y
+choco install curl --version 7.82.0 -y
+choco install awscli --version 2.5.4 -y
+choco install yarn --version 1.22.15 -y
+choco install golang --version 1.18.1 -y
+choco install azure-cli --version 2.35.0 -y
+choco install postman --version 9.15.2 -y
+choco install tortoisegit --version 2.13.0.1 -y
+choco install kubernetes-cli --version 1.23.5 -y
+choco install terraform --version 1.1.8 -y
+choco install docker-desktop --version 4.7.0 -y
+choco install pip --version 1.2.0 -y
